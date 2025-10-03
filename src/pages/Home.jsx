419e7d20750b5c60633062085e03d21207e62b69
minhhiep22102004-glitch <minhhiep22102004@gmail.com>
2025-10-04 05:00:24 +0700

    Update Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>🏠 Home Page</h2>
+      <p>Demo React Hooks + Router + Axios bằng Vite.</p>
+    </div>
+  );
+}
+ 
