3cedaa231783e952770b5a205afed9983333a7fb
minhhiep22102004-glitch <minhhiep22102004@gmail.com>
2025-10-04 05:00:10 +0700

    Update About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h2>ℹ️ About Page</h2>
+      <p>Trang này sử dụng React Router DOM để điều hướng.</p>
+    </div>
+  );
+}
